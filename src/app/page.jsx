1bdbf344e25f5fbf4c05b40f68b4161a5acdaab6
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-09-21 14:08:15 +0200

    analytics

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
 import Header from './components/Header/page'
 import MainContainer from './components/MainContainer/page'
+import Script from 'next/script'
+
 
 
 export default function Home({ children }) {
   return (
     <>
+       <div className="container">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-B7K7MZ0ZHF" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-B7K7MZ0ZHF');
+        `}
+      </Script>
+    </div>
       <Header />
       <MainContainer>
          {children}
